aadb1b12acb7e280a254fd14498b59d898b0a51c
Yuri Kilochek <yuri.kilochek@gmail.com>
2018-11-13 23:25:22 +0200

    Bring interface up.

--- include/interfaceptor/detail/virtual_interface_linux.hpp
+++ include/interfaceptor/detail/virtual_interface_linux.hpp
@@ -9,6 +9,7 @@
 #include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/asio/ip/network_v4.hpp>
 #include <boost/asio/ip/network_v6.hpp>
+#include <boost/asio/ip/udp.hpp>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -35,17 +36,37 @@ struct virtual_interface
     //void network(boost::asio::network_v6 const& network);
 
     void open(boost::system::error_code& ec) {
-        boost::asio::posix::stream_descriptor 
-            prototype{get_io_context(), ::open("/dev/net/tun", O_RDWR)};
+        boost::asio::posix::stream_descriptor
+            prototype(get_io_context(), ::open("/dev/net/tun", O_RDWR));
         if (!prototype.is_open()) {
             ec.assign(errno, boost::system::system_category());
             return;
         }
 
         ::ifreq ifr = {};
-        ifr.ifr_flags |= IFF_TUN;
+        ifr.ifr_flags = IFF_TUN;
         if (::ioctl(prototype.native_handle(),
                     TUNSETIFF,
+                    static_cast<void*>(&ifr)) == -1)
+        {
+            ec.assign(errno, boost::system::system_category());
+            return;
+        }
+
+        boost::asio::ip::udp::socket dummy_socket(get_io_context());
+        dummy_socket.open(boost::asio::ip::udp::endpoint().protocol(), ec);
+        if (ec) { return; }
+
+        if (::ioctl(dummy_socket.native_handle(),
+                    SIOCGIFFLAGS,
+                    static_cast<void*>(&ifr)) == -1)
+        {
+            ec.assign(errno, boost::system::system_category());
+            return;
+        }
+        ifr.ifr_flags |= IFF_UP | IFF_LOWER_UP;
+        if (::ioctl(dummy_socket.native_handle(),
+                    SIOCSIFFLAGS,
                     static_cast<void*>(&ifr)) == -1)
         {
             ec.assign(errno, boost::system::system_category());
@@ -58,7 +79,7 @@ struct virtual_interface
     }
 
     void open() {
-        boost::system::error_code ec;
+        auto ec = boost::system::error_code();
         open(ec);
         if (ec) { throw boost::system::system_error(ec); }
     }
