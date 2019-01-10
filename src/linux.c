b41d81684137423e1aa33e67a02d5b63dddecb13
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-01-11 00:03:46 +0200

    Native handle naming.

--- src/linux.c
+++ src/linux.c
@@ -8,8 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#include <linux/netlink.h>
 
 tuna_error_t
 tuna_create(tuna_device_t *device) {
@@ -36,17 +38,33 @@ tuna_create(tuna_device_t *device) {
         return TUNA_UNEXPECTED;
     }
 
-    device->priv_native_handle = fd;
+    device->priv_fd = fd;
     return 0;
 }
 
 tuna_error_t
 tuna_destroy(tuna_device_t *device) {
-    if (close(device->priv_native_handle) == -1) {
+    if (close(device->priv_fd) == -1) {
         return TUNA_UNEXPECTED;
     }
     return 0;
 }
+
+//static
+//tuna_error_t
+//tuna_priv_open_rtnl(int *fd) {
+//    int fg = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE);
+//    if (fd == -1) {
+//        return TUNA_UNEXPECTED;
+//    }
+//    return 0;
+//}
+//
+//TUNA_PRIV_API
+//tuna_error_t
+//tuna_get_name(tuna_device_t const *device, char *name, size_t *name_length) {
+//    tuna_error_t error = tuna_priv_open_netlink_socket();
+//}
 
 ///////////////////////////////////////////////////////////////////////////////
 #endif
