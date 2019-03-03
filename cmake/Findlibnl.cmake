4a9fe7f9702cb43744af7fb1e6ff803dbd182eca
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-03-03 14:23:20 +0200

    Don't rely on pkg-config to find libnl.

--- cmake/Findlibnl.cmake
+++ cmake/Findlibnl.cmake
@@ -1,23 +1,29 @@
-find_package(PkgConfig REQUIRED)
+include(FindPackageHandleStandardArgs)
 
-if(libnl_FIND_REQURIED)
-    set(_required REQUIRED)
-else()
-    set(_required)
-endif()
-
-if(libnl_FIND_QUIETLY)
-    set(_quiet QUIET)
-else()
-    set(_quiet)
-endif()
-
-pkg_check_modules(_libnl ${_required} ${_quiet} IMPORTED_TARGET
-    libnl-3.0
-    libnl-route-3.0
+find_path(_nl_INCLUDE_DIR
+    NAMES netlink/netlink.h
+    PATH_SUFFIXES libnl3
+)
+find_library(_nl_LIBRARY
+    NAMES nl-3
+)
+find_library(_nl-route_LIBRARY
+    NAMES nl-route-3
 )
 
-add_library(libnl::libnl INTERFACE IMPORTED)
-target_link_libraries(libnl::libnl INTERFACE
-    PkgConfig::_libnl
+find_package_handle_standard_args(libnl DEFAULT_MSG
+    _nl_LIBRARY
+    _nl-route_LIBRARY
+    _nl_INCLUDE_DIR
 )
+
+if(libnl_FOUND)
+    add_library(libnl::libnl INTERFACE IMPORTED)
+    target_include_directories(libnl::libnl INTERFACE
+        "${_nl_INCLUDE_DIR}"
+    )
+    target_link_libraries(libnl::libnl INTERFACE
+        "${_nl_LIBRARY}"
+        "${_nl-route_LIBRARY}"
+    )
+endif()
