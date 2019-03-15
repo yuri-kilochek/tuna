3ae522a124f35858840c3f61219b4dabbdfe407c
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-03-15 23:55:51 +0200

    Windows implementation stub.

---
+++ src/windows.c
@@ -0,0 +1,28 @@
+#include <tuna.h>
+
+#include <stddef.h>
+#include <windows.h>
+#include <tap-windows.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+extern
+char const *const tap_windows_inf_name;
+extern
+size_t const tap_windows_inf_size;
+extern
+unsigned char const *const tap_windows_inf_data;
+
+extern
+char const *const tap_windows_cat_name;
+extern
+size_t const tap_windows_cat_size;
+extern
+unsigned char const *const tap_windows_cat_data;
+
+extern
+char const *const tap_windows_sys_name;
+extern
+size_t const tap_windows_sys_size;
+extern
+unsigned char const *const tap_windows_sys_data;
