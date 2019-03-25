61495bf9b456846ab10d9f46cd487bb27611eb51
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-03-25 22:52:44 +0200

    Wrapped embedded TAP-Windows files in structs.

--- src/windows.c
+++ src/windows.c
@@ -1,28 +1,16 @@
 #include <tuna.h>
 
-#include <stddef.h>
-
 #include <windows.h>
 
 #include <tap-windows.h>
+#include <tap-windows-embedded-files.h>
 
 ///////////////////////////////////////////////////////////////////////////////
-
-extern char const *const pszTapWindowsInfFileName;
-extern size_t const cbTapWindowsInfFileSize;
-extern unsigned char const *const pTapWindowsInfFileData;
-
-extern char const *const pszTapWindowsCatFileName;
-extern size_t const cbTapWindowsCatFileSize;
-extern unsigned char const *const pTapWindowsCatFileData;
-
-extern char const *const pszTapWindowsSysFileName;
-extern size_t const cbTapWindowsSysFileSize;
-extern unsigned char const *const pTapWindowsSysFileData;
 
 tuna_error
 tuna_create_device(tuna_device **ppDevice)
 {
+    (void)g_TapWindowsEmbeddedInfFile;
     return 0;
 }
 
