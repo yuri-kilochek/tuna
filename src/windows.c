999bc18c973520c0e9e6ed26d0ee04288b658dca
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-03-29 00:04:42 +0200

    Enumerade registry adapter subkeys.

--- src/windows.c
+++ src/windows.c
@@ -1,23 +1,33 @@
 #include <tuna.h>
 
 #include <stdlib.h>
+#include <string.h>
 
 #include <windows.h>
 
 #include <tap-windows.h>
 #include <tap-windows-embedded-files.h>
 
+// DEBUG
+#include <stdio.h>
+
 ///////////////////////////////////////////////////////////////////////////////
 
-struct tuna_device {
+#pragma warning(push)
+#pragma warning(disable: 4996) // No, MSVC, C functions are not deprecated.
+
+struct tuna_device
+{
     HANDLE hFile;
 };
 
 static
 tuna_error
-TunaPrivTranslateErrCode(DWORD dwErrCode) {
-    switch (dwErrCode) {
-      case ERROR_SUCCESS:;
+TunaPrivTranslateErrCode(DWORD dwErrCode)
+{
+    switch (dwErrCode)
+    {
+      case 0:;
         return 0;
       default:;
         return TUNA_UNEXPECTED;
@@ -27,28 +37,57 @@ TunaPrivTranslateErrCode(DWORD dwErrCode) {
 tuna_error
 tuna_create_device(tuna_device **ppDevice)
 {
-    HKEY hkAdapters = 0;
+    tuna_device *pDevice = NULL;
+    HKEY hkAdapter = 0;
 
     DWORD dwErrCode;
     tuna_error err = 0;
 
-    tuna_device *pDevice = malloc(sizeof(*pDevice));
-    if (!pDevice) {
+    pDevice = malloc(sizeof(*pDevice));
+    if (!pDevice)
+    {
         err = TUNA_OUT_OF_MEMORY;
         goto fail;
     }
     *pDevice = (tuna_device){.hFile = INVALID_HANDLE_VALUE};
 
-    if ((dwErrCode = RegOpenKeyExA(HKEY_LOCAL_MACHINE,
-                                   ADAPTER_KEY,
-                                   0,
-                                   KEY_READ,
-                                   &hkAdapters)))
+    dwErrCode = RegOpenKeyExA(HKEY_LOCAL_MACHINE,
+                              ADAPTER_KEY,
+                              0,
+                              KEY_READ,
+                              &hkAdapter);
+    if (dwErrCode)
     {
-        hkAdapters = 0;
+        hkAdapter = 0;
         err = TunaPrivTranslateErrCode(dwErrCode);
         goto fail;
     }
+
+    char szSubkey[256];
+    strcpy(szSubkey, ADAPTER_KEY);
+    strcat(szSubkey, "\\");
+    DWORD dwSubkeyLen = (DWORD)strlen(szSubkey);
+    for (DWORD i = 0; ; ++i)
+    {
+        dwErrCode = RegEnumKeyExA(hkAdapter,
+                                  i,
+                                  szSubkey + dwSubkeyLen,
+                                  &(DWORD){sizeof(szSubkey) - dwSubkeyLen},
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  NULL);
+        if (dwErrCode)
+        {
+            if (dwErrCode == ERROR_NO_MORE_ITEMS) break;
+            err = TunaPrivTranslateErrCode(dwErrCode);
+            goto fail;
+        }
+
+
+
+    }
+
 
 
 
@@ -57,7 +96,7 @@ tuna_create_device(tuna_device **ppDevice)
     *ppDevice = pDevice;
 
   done:;
-    if (hkAdapters) { RegCloseKey(hkAdapters); }
+    if (hkAdapter) { RegCloseKey(hkAdapter); }
     return err;
   fail:;
     tuna_destroy_device(pDevice);
@@ -75,4 +114,4 @@ tuna_destroy_device(tuna_device *pDevice)
     free(pDevice);
 }
 
-
+#pragma warning(pop)
