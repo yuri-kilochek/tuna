e136e2d59e94534b744c3c9c48be35c70f1c5e8d
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-04-07 13:17:41 +0200

    Cleanup libnl find script.

--- cmake/Findlibnl.cmake
+++ cmake/Findlibnl.cmake
@@ -1,32 +1,22 @@
-find_path(_nl_INCLUDE_DIR
-    NAMES netlink/netlink.h
-    PATH_SUFFIXES libnl3
-)
-find_library(_nl_LIBRARY
-    NAMES nl-3
-)
-find_library(_nl-route_LIBRARY
-    NAMES nl-route-3
-)
+# No attempt is made to handle anything other than the invocation in 
+# /CMakeLists.txt
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args("libnl include directory"
-    REQUIRED_VARS _nl_INCLUDE_DIR
-)
-find_package_handle_standard_args("libnl binary"
-    REQUIRED_VARS _nl_LIBRARY
-)
-find_package_handle_standard_args("libnl-route binary"
-    REQUIRED_VARS _nl-route_LIBRARY
-)
-
-if(_nl_INCLUDE_DIR AND _nl_LIBRARY AND _nl-route_LIBRARY)
-    set(libnl_FOUND YES)
-
-    add_library(libnl::libnl INTERFACE IMPORTED)
-    target_include_directories(libnl::libnl INTERFACE "${_nl_INCLUDE_DIR}")
-    target_link_libraries(libnl::libnl INTERFACE
-        "${_nl_LIBRARY}"
-        "${_nl-route_LIBRARY}"
-    )
+find_path(_LIBNL_INCLUDE_DIR NAMES netlink/netlink.h PATH_SUFFIXES libnl3)
+if(NOT _LIBNL_INCLUDE_DIR)
+    message(FATAL_ERROR "Failed to find libnl include directory.")
 endif()
+
+find_library(_LIBNL nl-3)
+if(NOT _LIBNL)
+    message(FATAL_ERROR "Failed to find libnl.")
+endif()
+
+find_library(_LIBNL_ROUTE nl-route-3)
+if(NOT _LIBNL_ROUTE)
+    message(FATAL_ERROR "Failed to find libnl-route.")
+endif()
+
+add_library(libnl::libnl INTERFACE IMPORTED)
+target_include_directories(libnl::libnl INTERFACE "${_LIBNL_INCLUDE_DIR}")
+target_link_libraries(libnl::libnl INTERFACE "${_LIBNL}" "${_LIBNL_ROUTE}")
+set(libnl_FOUND YES)
