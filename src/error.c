b8dd89fe4aa4dbc5ad442a36a6f85dc56d085de5
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-06-23 20:09:19 +0300

    Code style.

--- src/error.c
+++ src/error.c
@@ -3,33 +3,33 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 char const*
-tuna_get_error_name(tuna_error err) {
-    switch (err) {
-      case 0:;
-        return "none";
-      case TUNA_UNEXPECTED:;
+tuna_get_error_name(tuna_error error) {
+    switch (error) {
+    case 0:
+        return "0";
+    case TUNA_UNEXPECTED:
         return "unexpected";
-      case TUNA_DEVICE_LOST:;
+    case TUNA_DEVICE_LOST:
         return "device lost";
-      case TUNA_FORBIDDEN:;
+    case TUNA_FORBIDDEN:
         return "forbidden";
-      case TUNA_OUT_OF_MEMORY:;
+    case TUNA_OUT_OF_MEMORY:
         return "out of memory";
-      case TUNA_TOO_MANY_HANDLES:;
+    case TUNA_TOO_MANY_HANDLES:
         return "too many handles";
-      case TUNA_DEVICE_BUSY:;
+    case TUNA_DEVICE_BUSY:
         return "device busy";
-      case TUNA_NAME_TOO_LONG:;
+    case TUNA_NAME_TOO_LONG:
         return "name too long";
-      case TUNA_DUPLICATE_NAME:;
+    case TUNA_DUPLICATE_NAME:
         return "duplicate name";
-      case TUNA_INVALID_NAME:;
+    case TUNA_INVALID_NAME:
         return "invalid name";
-      case TUNA_MTU_TOO_SMALL:;
+    case TUNA_MTU_TOO_SMALL:
         return "mtu too small";
-      case TUNA_MTU_TOO_BIG:;
+    case TUNA_MTU_TOO_BIG:
         return "mtu too big";
-      default:;
+    default:
         return NULL;
     }
 }
