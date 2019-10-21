eb9aca3b3f9b807d1f27ca7de8129b486608d750
Yuri Kilochek <yuri.kilochek@gmail.com>
2019-10-21 22:10:51 +0300

    Refactor WIP.

--- src/linux.c
+++ src/linux.c
@@ -10,8 +10,6 @@
 #include <linux/if.h>
 #include <linux/if_tun.h>
 
-#include <netlink/route/addr.h>
-
 ///////////////////////////////////////////////////////////////////////////////
 
 static
--- src/linux/address_list.c
+++ src/linux/address_list.c
@@ -1,5 +1,7 @@
 #include <tuna/priv/linux.h>
 
+#include <netlink/route/addr.h>
+
 #include <stddef.h>
 #include <string.h>
 
